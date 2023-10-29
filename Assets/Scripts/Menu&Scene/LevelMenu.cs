0b0d175f6f0f5ccef87ae4968a432b2eb890ec59
Lee Clarifiedfish <jieyvlee@qq.com>
2023-10-29 10:43:35 +0800

    修改LevelScene0报错，修改背景图位置

--- Assets/Scripts/Menu&Scene/LevelMenu.cs
+++ Assets/Scripts/Menu&Scene/LevelMenu.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LevelMenu : MainMenu//MonoBehaviour
+public class LevelMenu : MonoBehaviour
 {
+    public Button soundButton;
+    //调用mainMenu中的isMuOn？应该从调用UIcountrol中调用
+    public static bool isMuOn = true;
+    public Sprite muOnImg;
+    public Sprite muOffImg;
+    void Start()
+    {
+        soundButton ??= GameObject.Find("MusOnBtn").GetComponent<Button>();
+        soundButton.onClick.AddListener(SwitchSound);
+    }
+    public void SwitchSound()
+    {
+        if (isMuOn)
+        {
+            isMuOn = false;
+            //shut down music//
+            UnityEngine.Debug.Log("shut down music" + isMuOn);
+            soundButton.GetComponent<UnityEngine.UI.Image>().sprite = muOffImg;
+            return;
+        }
+        else
+        {
+            isMuOn = true;
+            //turn on music//
+            UnityEngine.Debug.Log("turn on music" + isMuOn);
+            soundButton.GetComponent<UnityEngine.UI.Image>().sprite = muOnImg;
+            return;
+        }
 
+    }
 }
